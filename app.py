44b5ef60c681fcc5fd4938da9a66e4cca7954830
NikosNtafopoulos <niklausblockchaindev@yahoo.com>
2019-01-01 20:23:30 +0200

    routes with flask forms

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask,render_template,request,redirect,url_for
+from flask import Flask,render_template,request,redirect,url_for,flash
 from flask_sqlalchemy import SQLAlchemy
+from forms import ClientForm,ClientLoginForm
 
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://test:test@localhost/test'
+#security for modified cookies,sessions etc
+app.config['SECRET_KEY']= '8810fc810e8c15c03186e3cbb695fb92'
 db = SQLAlchemy(app)
 #DB_USER:PASSWORD@HOST/DATABASE
 
@@ -24,17 +27,44 @@ class Client(db.Model):
 def index():
     return render_template('index.html')
 
+# Render the html form
 @app.route('/add')
 def add():
     return render_template('add.html')
 
-#post
+# Post route to database from the html form
 @app.route('/post_user',methods=['POST'])
 def post_user():
     user = Client(request.form['first_name'],request.form['last_name'])
     db.session.add(user)
     db.session.commit()
     return redirect(url_for('index'))
+
+'''
+    ROUTES FROM forms.py
+    python forms with classes and templating
+'''
+@app.route('/register',methods=['GET','POST'])
+def register():
+    form = ClientForm()
+    if form.validate_on_submit():
+        flash(f'User {form.first_name.data} {form.last_name.data}has been created')
+        return redirect(url_for('index'))
+    else:
+        flash('Failed')
+    return render_template('register.html',form=form)
+
+#login route
+@app.route('/login',methods=['GET','POST'])
+def login():
+    form = ClientLoginForm()
+    if form.validate_on_submit():
+        flash('Welcome')
+        return redirect(url_for('index'))
+    else:
+        flash('Login Unsuccessful')
+    return render_template('login.html',form=form)
+
 
 if __name__== "__main__":
     app.run(debug=True)
