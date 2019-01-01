d38de0c72da4fcf1b0385f7654ac78a0e21c98a4
NikosNtafopoulos <niklausblockchaindev@yahoo.com>
2019-01-01 21:23:02 +0200

    UserInfo model table

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask,render_template,request,redirect,url_for,flash
 from flask_sqlalchemy import SQLAlchemy
 from forms import ClientForm,ClientLoginForm
+from datetime import datetime
 
 
 app = Flask(__name__)
@@ -11,6 +12,8 @@ app.config['SECRET_KEY']= '8810fc810e8c15c03186e3cbb695fb92'
 db = SQLAlchemy(app)
 #DB_USER:PASSWORD@HOST/DATABASE
 
+# DB MODEL
+# Test model table for route /post_user with html inputs
 class Client(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(80), unique=True)
@@ -22,6 +25,20 @@ class Client(db.Model):
 
     def __repr__(self):
         return '{}  {}'.format(self.first_name, self.last_name)
+
+# test table for id,firstName,lastName,image,email,password,dateCreated
+class UserInfo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(80), unique=True)
+    last_name = db.Column(db.String(100), unique=True)
+    image = db.Column(db.String(20), nullable=False,default='default.jpg')
+    email = db.Column(db.String(30), unique=True,nullable=False)
+    password = db.Column(db.String(100), unique=True, nullable=False)
+    date_created = db.Column(db.DateTime,nullable=False,default=datetime.utcnow())
+
+    def __repr__(self):
+        return f"UserInfo('User: {self.first_name} {self.last_name}')"
+
 #routes
 @app.route('/')
 def index():
